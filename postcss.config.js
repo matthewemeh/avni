d45fb1c816f46eec1d67791fbb9d73784c4798f1
matthewemeh <mcmekus10@gmail.com>
2022-12-27 15:30:57 +0100

    Avni Begins: First Commit

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {},
+    purgecss: {
+      content: [],
+      defaultExtractor: [],
+      safelist: [],
+    },
+  },
+};
