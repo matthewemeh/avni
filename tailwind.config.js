696ae9c40b22a613287a90ddf31b5427a467d0aa
matthewemeh <mcmekus10@gmail.com>
2023-01-03 16:46:47 +0100

    added colors, font-family,screens to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        alto: '#d9d9d9',
+        shark: '#1b1c1e',
+        firefly: '#172c34',
+        romance: '#fffefd',
+        'cod-gray': '#111111',
+        'wild-sand': '#f5f5f5',
+        'dove-gray': '#717171',
+        'alto-light': '#cecece',
+        'outer-space': '#232b2b',
+        'muddy-waters': '#b68e5c',
+        'azure-radiance': '#0381f9',
+      },
     },
-    fontFamily: {},
-    screens: {},
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    screens: { '1150px': { max: '1150px' } },
   },
-  plugins: [],
 };
