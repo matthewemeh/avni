f9a3e7781f1f986375e882bde31a8acbc79378f4
matthewemeh <mcmekus10@gmail.com>
2023-01-03 18:37:44 +0100

    refactored Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,11 +8,9 @@ import Youtube from './icons/Youtube';
 import Instagram from './icons/Instagram';
 
 const Footer = () => {
-  const email = 'furniture@avniverse.com';
-
   return (
     <footer className='bg-wild-sand pt-[100px] -mx-[19.4%] px-[20%] overflow-hidden dark:bg-[#161718]'>
-      <div className='w-full grid grid-cols-5 gap-x-[5%] pb-[100px] font-medium border-alto-light border-b-[1px] dark:border-wild-sand'>
+      <div className='w-full text-[12px] leading-[30px] grid grid-cols-5 gap-x-[5%] pb-[100px] font-medium border-alto-light border-b-[1px] dark:border-wild-sand'>
         <div>
           <Logo />
           <p className='mt-[50px] text-[12px] leading-[18px]'>Follow Us</p>
@@ -40,79 +38,43 @@ const Footer = () => {
 
         <div className='grid grid-rows-[auto]'>
           <p className='font-semibold text-[16px] leading-6'>Things we care about</p>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Ecofriendly furnitures
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Creative verse
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Discovery cards/shorts
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            AR & AI for seamless experience
-          </Link>
+          <Link href='/'>Ecofriendly furnitures</Link>
+          <Link href='/'>Creative verse</Link>
+          <Link href='/'>Discovery cards/shorts</Link>
+          <Link href='/'>AR & AI for seamless experience</Link>
         </div>
 
         <div className='grid grid-rows-[auto]'>
           <p className='font-semibold text-[16px] leading-6'>Company</p>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            About Avni.furniture
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Join our team
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Ethics and Integrity
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Collaborators login
-          </Link>
+          <Link href='/'>About Avni.furniture</Link>
+          <Link href='/'>Join our team</Link>
+          <Link href='/'>Ethics and Integrity</Link>
+          <Link href='/'>Collaborators login</Link>
         </div>
 
         <div className='grid grid-rows-[auto]'>
           <p className='font-semibold text-[16px] leading-6'>Social</p>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Pinterest
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Facebook
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Twitter
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Medium
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            Snapchat
-          </Link>
+          <Link href='/'>Pinterest</Link>
+          <Link href='/'>Facebook</Link>
+          <Link href='/'>Twitter</Link>
+          <Link href='/'>Medium</Link>
+          <Link href='/'>Snapchat</Link>
         </div>
 
         <div className='grid grid-rows-[auto]'>
           <p className='font-semibold text-[16px] leading-6'>Avni.Furniture</p>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            For users
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            For furniture designers
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            For furniture manufacturers
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            For interior designers
-          </Link>
-          <Link className='text-[12px] leading-[30px]' href='/'>
-            For institutions, companies, etc
-          </Link>
+          <Link href='/'>For users</Link>
+          <Link href='/'>For furniture designers</Link>
+          <Link href='/'>For furniture manufacturers</Link>
+          <Link href='/'>For interior designers</Link>
+          <Link href='/'>For institutions, companies, etc</Link>
         </div>
       </div>
 
       <div className='relative mt-14 mb-14 flex items-center gap-x-5 font-normal text-[12px] leading-[18px] text-firefly dark:text-wild-sand'>
         <span>&copy; Avni 2022</span>
-        <Link href={`mailto:${email}`} target='_blank'>
-          {email}
+        <Link href='mailto:furniture@avniverse.com' target='_blank'>
+          furniture@avniverse.com
         </Link>
         <Link href='/'>Privacy Policy</Link>
         <Link href='/'>Terms</Link>
