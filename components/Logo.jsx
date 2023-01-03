c6e2f63a2c18c02cd5230bfc0ed4897478bce7ba
matthewemeh <mcmekus10@gmail.com>
2023-01-03 18:36:36 +0100

    refactored Logo component

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,28 +1,5 @@
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-
-import logoDarkMode from '../public/assets/pngs/avni-full-blue-white.png';
-import logoLightMode from '../public/assets/pngs/avni-full-blue-black.png';
-
-const Logo = () => {
-  const [logo, setLogo] = useState(logoLightMode);
-
-  // componentDidMount
-  useEffect(() => {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-      if (e.matches) {
-        setLogo(logoDarkMode);
-        localStorage.isDarkMode = 'true';
-      } else {
-        setLogo(logoLightMode);
-        localStorage.isDarkMode = 'false';
-      }
-    });
-
-    setLogo(localStorage.isDarkMode == 'true' ? logoDarkMode : logoLightMode);
-  }, []);
-
-  return <Image src={logo} width={80} height={20} alt='avni logo' />;
-};
+const Logo = () => (
+  <div className='bg-[url(/assets/pngs/avni-full-blue-black.png)] bg-contain w-20 h-5 bg-no-repeat bg-center dark:bg-[url(/assets/pngs/avni-full-blue-white.png)]' />
+);
 
 export default Logo;
