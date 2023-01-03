3b4b7c092d4f93d81e9481621173850d14178bd3
matthewemeh <mcmekus10@gmail.com>
2023-01-03 16:55:39 +0100

    updated root

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,17 @@
-import '../styles/main.css';
+// import '../styles/globals.css'; // for development
+import '../styles/main.css'; // for production
+
+// import { Montserrat } from '@next/font/google';
+
+import Navigation from '../components/Navigation';
+
+// const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <Navigation />
+      <Component {...pageProps} />
+    </main>
+  );
 }
