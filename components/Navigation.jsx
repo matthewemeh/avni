2933ccbc31a2d2ab8cb5478a1ec7516ff4711322
matthewemeh <mcmekus10@gmail.com>
2023-01-12 03:12:07 +0100

    made navigation filled and sticky

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
   const { menuOpened, setMenuOpened } = useContext(AppContext);
 
   return (
-    <nav className='py-7 px-10 w-full flex items-center justify-between outline-0 z-10'>
+    <nav className='py-7 px-10 w-full flex items-center justify-between sticky top-0 bg-white outline-0 z-10 dark:bg-shark'>
       <Logo />
       <button onClick={() => setMenuOpened(!menuOpened)} className='block border-y-2 w-5 h-3' />
     </nav>
