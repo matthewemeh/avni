cde1dc1425df51ea96cd4852b780537cf7079bbf
matthewemeh <mcmekus10@gmail.com>
2023-01-16 20:50:42 +0100

    fixed text wraps issues. Added href, title fields

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,9 +29,9 @@ const SLIDES_DATA = [
     video: null,
     title: 'Shop now',
     focalPoints: [
-      { x: 20, y: 54 },
-      { x: 35, y: 60 },
-      { x: 70, y: 48 },
+      { x: 20, y: 54, title: 'tables', href: '/' },
+      { x: 35, y: 60, title: 'chairs', href: '/' },
+      { x: 70, y: 48, title: 'dinning', href: '/' },
     ],
     themeColor: '#232b2b',
   },
@@ -50,9 +50,9 @@ const SLIDES_DATA = [
     video: null,
     title: 'Getting real with us',
     focalPoints: [
-      { x: 12, y: 63 },
-      { x: 50, y: 5 },
-      { x: 85, y: 33 },
+      { x: 12, y: 63, title: 'faq', href: '/' },
+      { x: 50, y: 5, title: 'reach out', href: '/' },
+      { x: 85, y: 33, title: 'social handles', href: '/' },
     ],
     themeColor: '#187789',
   },
@@ -100,8 +100,8 @@ export default function Home({ slidesData, articles }) {
         <Preloader />
 
         <header className='mt-[89px] phones:mt-[61px]'>
-          <h1 className='text-[42px] leading-[60px] text-center max-w-[400px] mx-auto laptops:text-[36px] laptops:max-w-[357px] phones:max-w-[330px] phones:text-[32px] phones:leading-[50px]'>
-            A mission to get your perfect piece
+          <h1 className='text-[42px] leading-[60px] text-center max-w-[500px] mx-auto laptops:text-[36px] laptops:max-w-[450px] phones:max-w-[380px] phones:text-[32px] phones:leading-[50px]'>
+            Inspiring creativity for your perfect space
           </h1>
 
           <LinkBubble text='Go Shopping' href='/home' />
@@ -115,7 +115,7 @@ export default function Home({ slidesData, articles }) {
           <HistorySlider />
 
           <section className='mt-[200px] phones:mt-[103px]'>
-            <h3 className='text-[30px] leading-[45px] text-center laptops:text-[25px] phones:text-center phones:mx-auto phones:max-w-[70%]'>
+            <h3 className='text-[30px] leading-[45px] text-center laptops:text-[25px] phones:text-center phones:mx-auto phones:max-w-full'>
               Furniture solution on a budget
             </h3>
             <div className='bg-[url(/assets/pngs/furniture1.png)] bg-cover mt-[84px] h-[560px] w-full bg-no-repeat bg-center flex items-end p-10 laptops:h-[410px] laptops:pb-[31px] phones:p-5'>
@@ -125,11 +125,11 @@ export default function Home({ slidesData, articles }) {
             </div>
 
             <div className='mt-[200px] bg-[url(/assets/pngs/furniture2.png)] w-full h-[560px] bg-cover bg-center bg-no-repeat flex items-end laptops:h-[410px] phones:mt-[103px]'>
-              <div className='bg-[rgba(0,0,0,0.2)] h-[188px] backdrop-blur-[72px] flex items-center justify-between w-full py-[55px] px-[60px] gap-y-5 laptops:h-[108px] laptops:py-[30px] laptops:px-10 phones:h-[142px] phones:flex-col phones:px-7'>
+              <div className='bg-[rgba(0,0,0,0.2)] h-[188px] backdrop-blur-[72px] flex items-center justify-between w-full py-[55px] px-[60px] gap-y-5 laptops:h-[108px] laptops:py-[30px] laptops:px-10 phones:pt-5 phones:min-h-[142px] phones:flex-col phones:px-5 phones:text-center'>
                 <p className='font-semibold text-[32px] leading-[39px] max-w-[380px] text-white laptops:text-[20px] laptops:leading-[24px] laptops:max-w-[240px] phones:max-w-none'>
                   Ideas for a cool living space
                 </p>
-                <button className='bg-white rounded-full text-cod-gray font-medium text-[12px] leading-4 py-[15px] px-[30px] phones:py-[11px] phones:px-[25px]'>
+                <button className='bg-white rounded-full text-cod-gray font-medium text-[12px] leading-4 py-[15px] px-[30px] phones:py-[12px] phones:px-[30px]'>
                   Visit
                 </button>
               </div>
@@ -150,7 +150,12 @@ export default function Home({ slidesData, articles }) {
             <LinkBubble text="Here's how" href='/home' />
 
             <div className='w-full h-[560px] mt-[84px] mb-[26px] laptops:h-[410px] phones:h-[191px]'>
-              <Image src={newsHeadMedia} alt='avni news header' className='w-full h-full' />
+              <Image
+                priority
+                src={newsHeadMedia}
+                alt='avni news header'
+                className='w-full h-full'
+              />
             </div>
 
             <CurlyArrow />
