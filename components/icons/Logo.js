e2cf0f37e342f97e657100ec74fe6c6a13787990
matthewemeh <mcmekus10@gmail.com>
2023-01-19 04:40:54 +0100

    moved logo component into icons folder

---
+++ components/icons/Logo.js
@@ -0,0 +1,5 @@
+const Logo = () => (
+  <div className='bg-[url(/assets/webps/avni-full-blue-black.webp)] bg-contain w-20 h-5 bg-no-repeat bg-center dark:bg-[url(/assets/webps/avni-full-blue-white.webp)]' />
+);
+
+export default Logo;
