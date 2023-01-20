0edef79d7414a350bd79bb03b812c68911424770
matthewemeh <mcmekus10@gmail.com>
2023-01-20 02:37:24 +0100

    added errors 404 and 500 pages

---
+++ components/Error.js
@@ -0,0 +1,45 @@
+import { useRef } from 'react';
+import LinkBubble from '../components/LinkBubble';
+
+import { addClass, removeClass } from '../public/utils';
+
+const Error = ({ errorCode, errorMessage, alertMessage, redirectLink, redirectText }) => {
+  const msgRef = useRef();
+
+  const closeMessage = () => {
+    removeClass(msgRef.current, 'top-1');
+    addClass(msgRef.current, '-top-[200px]');
+  };
+  return (
+    <main className='relative font-medium'>
+      <div className='grid grid-cols-[25%_75%] w-[261px] h-[103px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <h1 className='font-poppins font-semibold text-[22px] leading-[33px] border-r-[1px] border-dove-gray flex items-center'>
+          {errorCode}
+        </h1>
+
+        <p className='font-poppins text-[12px] leading-[18px] text-light-dove-gray flex items-center justify-end dark:text-wild-sand'>
+          {errorMessage}
+        </p>
+
+        <LinkBubble
+          href={redirectLink}
+          text={redirectText}
+          extraStyles={{ gridColumnStart: 1, gridColumnEnd: 3 }}
+        />
+      </div>
+
+      <div
+        ref={msgRef}
+        className='text-white flex gap-x-[10px] items-center justify-center transition-all duration-500 text-[16px] leading-5 bg-red-orange rounded-md py-[14px] px-[25px] whitespace-nowrap absolute top-1 left-1/2 -translate-x-1/2 delay-300 animate-[pop-down_800ms_ease-in-out] laptops:w-[85%] laptops:whitespace-normal laptops:text-center phones:w-[90%] phones:text-[14px] phones:leading-[17px] x-small-phones:px-3'
+      >
+        {alertMessage}
+        <button
+          onClick={closeMessage}
+          className='bg-[url(/assets/svgs/close2.svg)] bg-no-repeat bg-center w-4 h-4 min-w-[16px]'
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Error;
