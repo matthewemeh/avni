fa6625808582ead4c8486cf64613ced32b4a576e
matthewemeh <mcmekus10@gmail.com>
2023-01-28 02:16:38 +0100

    made home search stretchable

--- components/HomeSearch.js
+++ components/HomeSearch.js
@@ -1,15 +1,15 @@
 const HomeSearch = () => {
   return (
-    <div className='relative'>
+    <div className='relative w-full'>
       <div className='absolute inline left-[25px] bg-[url(/assets/svgs/search.svg)] bg-center bg-no-repeat w-[46px] h-full' />
 
       <input
         type='text'
         placeholder='What do you desire?'
-        className='w-[720px] bg-wild-sand h-[46px] rounded-[100px] pl-[70px] outline-0'
+        className='w-full bg-wild-sand h-[46px] rounded-[100px] pl-[70px] outline-0'
       />
 
-      <div className='absolute inline right-[13px] bg-[url(/assets/svgs/camera.svg)] bg-center bg-no-repeat w-[46px] h-full' />
+      <div className='cursor-pointer absolute inline right-[13px] bg-[url(/assets/svgs/camera.svg)] bg-center bg-no-repeat w-[46px] h-full' />
     </div>
   );
 };
