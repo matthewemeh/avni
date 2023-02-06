ed12ff4bc780fdbccef31e6c1a0c6cb8c6c18668
matthewemeh <mcmekus10@gmail.com>
2023-02-06 22:21:03 +0100

    added new alert info, pushed a state up to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,13 +12,21 @@ export default function App({ Component, pageProps }) {
   const LAPTOP_BREAKPOINT = 1024;
   const SMALL_MOBILE_BREAKPOINT = 400;
   const [screenWidth, setScreenWidth] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
 
   // componentDidMount
   useEffect(() => setScreenWidth(window.screen.availWidth), []);
 
   return (
     <AppContext.Provider
-      value={{ screenWidth, MOBILE_BREAKPOINT, LAPTOP_BREAKPOINT, SMALL_MOBILE_BREAKPOINT }}
+      value={{
+        menuOpened,
+        screenWidth,
+        setMenuOpened,
+        MOBILE_BREAKPOINT,
+        LAPTOP_BREAKPOINT,
+        SMALL_MOBILE_BREAKPOINT,
+      }}
     >
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
@@ -28,6 +36,12 @@ export default function App({ Component, pageProps }) {
         <Script id='unload-script'>{`window.onbeforeunload = () => window.scrollTo(0, 0);`}</Script>
 
         <Component {...pageProps} />
+
+        <div
+          id='alert'
+          style={{ bottom: '-100px' }}
+          className='fixed left-1/2 -translate-x-1/2 w-max max-w-[80vw] p-4 bg-transparent text-white mb-8 rounded-md text-[1rem] transition-all duration-500 ease-in-out'
+        />
       </main>
     </AppContext.Provider>
   );
